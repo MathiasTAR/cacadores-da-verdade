b0edd34096c8e1fb71cbf4991a90284ff1cd9fa7
Mathias <mathiasaraujo508@gmail.com>
2025-11-14 23:46:33 -0300

    sistema inventario

--- Cenas/cidade.gd
+++ Cenas/cidade.gd
@@ -59,7 +59,7 @@ var _dialog_data: Dictionary = {
 
 func _process(delta: float) -> void:
 	
-	if Input.is_action_just_pressed("ui_select"):
+	if Input.is_action_just_pressed("advance_message"):
 		var _new_dialog: TelaDialogo = _TELAPEQUENADIALOGO.instantiate()
 		_new_dialog.data = _dialog_data
 		_hud.add_child(_new_dialog)
